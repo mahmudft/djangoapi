a66751bb09ad1193a78c2cae5b8595297c8aeb1b
Mahmud <fatullayevm@gmail.com>
2019-11-15 16:26:49 +0400

    react front

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -54,14 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = [
-#    'http://localhost:3000',
-#]
-#
-#CORS_ORIGIN_REGEX_WHITELIST = [
-#]
+
 
 ROOT_URLCONF = 'marketplace.urls'
 
